aa1c00b5f690afa506327920c1aab5a2f1398357
bshankar <ebhavanishankar@gmail.com>
2023-02-19 17:57:08 +0530

    Add fastapi scaffolding and hello world API

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-POC for HOTOSM FMTM project.
+# POC for HOTOSM FMTM project.
 
 - The [`api`](./api) folder has the code for the Python backend.
 - The [`ui`](./ui) folder has the code for the React frontend.
+
+# Running the backend
+
+```
+$ cd api
+$ pdm venu activate
+$ uvicorn main:app
+```
---
+++ api/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Python dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = inputs @ { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rnix-lsp
+          python310
+          pdm
+          python310Packages.python-lsp-server
+          postgresql15Packages.postgis
+        ];
+      };
+    };
+}
