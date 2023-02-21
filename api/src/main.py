80c129a7ad9ed4bbf61942894b4b2c7f63ed6359
Akshay Gupta <akshaygrkesh@gmail.com>
2023-02-22 00:43:07 +0530

    feat: Add project boundary API

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+import json
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from model.database import subdivide
 
 app = FastAPI()
 
+class Geometry(BaseModel):
+    coordinates: list
+    type: str
+
+class Feature(BaseModel):
+    type: str
+    geometry: Geometry
+
+class Boundary(BaseModel):
+    type: str
+    features: list[Feature]
+
+class Params(BaseModel):
+    subTasks: int
+    splitBy: list[str]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/project")
+async def project(boundary: Boundary, params: Params):
+    return subdivide(boundary, params)
