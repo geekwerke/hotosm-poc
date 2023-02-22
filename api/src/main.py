774c6b98892abc6e97978375c36342cfe998c76d
Ajith Ranka <ajithranka@users.noreply.github.com>
2023-02-22 06:57:18 +0000

    Add cors middleware

--- api/src/main.py
+++ api/src/main.py
@@ -1,9 +1,23 @@
 import json
 from fastapi import Body, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model.database import subdivide
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Geometry(BaseModel):
     coordinates: list
